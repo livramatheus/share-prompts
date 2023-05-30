beb0e59f13789289c4a29e8d87863b65b5c8079a
livramateus <livra.matheus@gmail.com>
2023-05-29 21:20:04 -0300

    refactor: remove unused import

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth/next";
-// import nextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
